67d29cbcda8fbc9e741570a370018e9fc399ae4d
Alan Languirand <alan@13protons.com>
2019-08-16 22:29:58 -0400

    let dyno sleep when no clients connected, also API to show connection status

--- index.js
+++ index.js
@@ -14,51 +14,74 @@ let vehicles = {};
 
 server.listen(PORT);
 
+app.get('/api/status', function(req, res) {
+  getConfig().then(function (data) {
+    if (timeBounds(data.schedule)) {
+      res.json({
+        status: 'running',
+        running: true,
+      })
+    } else {
+      res.json({
+        status: 'out of service',
+        running: false,
+      })
+    }
+  });
+})
 app.use('/', express.static('./public'));
+
+let listener = undefined;
 
 io.on('connection', function (socket) {
   // ensure polling
+  if (!listener) {
+    console.log('setup polling!');
+    pollForLocation();
+    listener = setTimeout(pollForLocation, 5000);
+  }
+
+  socket.on('disconnect', (reason) => {
+    io.of('/').clients((err, clients) => {
+      if (err) throw err;
+      if (!clients.length) {
+        console.log('no more clients');
+        listener = undefined;
+      }
+    })
+  });
 });
 
-setInterval(pollForLocation, 5000);
-
 function pollForLocation() {
+  io.of('/').clients((err, clients)=>{
+    if (err) throw err;
+    if (clients.length) {
+      console.log(`${clients.length} clients connected`);
+      updateVehicleLocations();
+    }
+  })
+}
+
+function updateVehicleLocations() {
   getConfig().then(function (data) {
     console.log('config data', data);
     // in time window?
     if (timeBounds(data.schedule)) {
       // try to get locations (within last 10 min)
       assetLocations(data.org, data.devices)
-        .then(function(updates){
-          updates.forEach(function(item) {
+        .then(function (updates) {
+          updates.forEach(function (item) {
             // broadcast most recent ones
             io.emit('vehicleUpdate', item)
           })
         })
-        .catch((function(err){
+        .catch((function (err) {
           console.log('failed to get device info', err)
         }))
+    } else {
+      listener = setTimeout(pollForLocation, 1000 * 60);
     }
-  })
-}
-
-
-
-function vehicleData(serial) {
-  let data = vehicles[serial];
-  if (!data) {
-    throw `No data for device ${serial}`;
-  }
-
-  return {
-    coords: {
-      lng: data.longitude,
-      lat: data.latitude
-    },
-    name: data.name,
-    serial: data.serial,
-    updated: data.time
-  }
+  });
 }
 
 
